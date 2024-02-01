4bcea03797d5b6b0a574292dc4d92f5dc595adc6
AlbeTorres <albertocorreoficial@gmail.com>
2024-01-31 21:58:02 -0500

    feat: add filter to github project fetching

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,81 +1,81 @@
 export const contact = [
-    {
-      bg: "bg-pink-500",
-      img: "instagram.svg",
-      text: "instagram",
-      link: "https://www.instagram.com/albe_torres/",
-    },
-    {
-      bg: "bg-white",
-      img: "x.svg",
-      text: "x",
-      link: "https://twitter.com/albe_torres",
-    },
-    {
-      bg: "bg-blue-500",
-      img: "linkedin.svg",
-      text: "linkedin",
-      link: "https://www.linkedin.com/in/albetorres/",
-    },
-    {
-      bg: "bg-blue-500",
-      img: "github.svg",
-      text: "github",
-      link: "https://github.com/AlbeTorres",
-    },
-  ];
-  
-  export const skills = [
-    {
-      bg: "bg-yellow-500",
-      img: "javascript.svg",
-      text: "Javascript",
-    },
-    {
-      bg: "bg-blue-500",
-      img: "typescript.svg",
-      text: "typescript",
-    },
-    {
-      bg: "bg-green-500",
-      img: "node.svg",
-      text: "node.JS",
-    },
-    {
-      bg: "bg-green-300",
-      img: "mongodb.svg",
-      text: "mongodb",
-    },
-    {
-      bg: "bg-blue-300",
-      img: "mysql.svg",
-      text: "mysql",
-    },
-    {
-      bg: "bg-gray-500",
-      img: "postgresql.svg",
-      text: "postgresql",
-    },
-    {
-      bg: "bg-cyan-500",
-      img: "react.svg",
-      text: "react",
-    },
-    {
-      bg: "bg-white",
-      img: "next.svg",
-      text: "next",
-    },
-    {
-      bg: "bg-pink-500",
-      img: "astro.svg",
-      text: "astro",
-    },
-    {
-      bg: "bg-blue-500",
-      img: "tailwindcss.svg",
-      text: "tailwindcss",
-    },
-  ];
+  {
+    bg: 'bg-pink-500',
+    img: 'instagram.svg',
+    text: 'instagram',
+    link: 'https://www.instagram.com/albe_torres/',
+  },
+  {
+    bg: 'bg-white',
+    img: 'x.svg',
+    text: 'x',
+    link: 'https://twitter.com/albe_torres',
+  },
+  {
+    bg: 'bg-blue-500',
+    img: 'linkedin.svg',
+    text: 'linkedin',
+    link: 'https://www.linkedin.com/in/albetorres/',
+  },
+  {
+    bg: 'bg-blue-500',
+    img: 'github.svg',
+    text: 'github',
+    link: 'https://github.com/AlbeTorres',
+  },
+]
 
-  
+export const skills = [
+  {
+    bg: 'bg-yellow-500',
+    img: 'javascript.svg',
+    text: 'Javascript',
+  },
+  {
+    bg: 'bg-blue-500',
+    img: 'typescript.svg',
+    text: 'typescript',
+  },
+  {
+    bg: 'bg-green-500',
+    img: 'node.svg',
+    text: 'node.JS',
+  },
+  {
+    bg: 'bg-green-300',
+    img: 'mongodb.svg',
+    text: 'mongodb',
+  },
+  {
+    bg: 'bg-blue-300',
+    img: 'mysql.svg',
+    text: 'mysql',
+  },
+  {
+    bg: 'bg-gray-500',
+    img: 'postgresql.svg',
+    text: 'postgresql',
+  },
+  {
+    bg: 'bg-cyan-500',
+    img: 'react.svg',
+    text: 'react',
+  },
+  {
+    bg: 'bg-white',
+    img: 'next.svg',
+    text: 'next',
+  },
+  {
+    bg: 'bg-pink-500',
+    img: 'astro.svg',
+    text: 'astro',
+  },
+  {
+    bg: 'bg-blue-500',
+    img: 'tailwindcss.svg',
+    text: 'tailwindcss',
+  },
+]
+
+export const project = [549815016, 545719459]
