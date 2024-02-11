f702a7eda5d192c0faf1799912572bb57e8af818
AlbeTorres <albertocorreoficial@gmail.com>
2024-02-11 12:39:54 -0500

    feat: add nabvar in movile view

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode:"class",
-	theme: {
-		extend: {
-			backgroundImage:{
-				"open-menu":"url('open-menu.svg')",
-				"close-menu":"url('close-menu.svg')"
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundColor: { graydark: '#0d1117' },
+      backgroundImage: {
+        'open-menu': "url('./src/icons/open-menu.svg')",
+        'close-menu': "url('./src/icons/close-menu.svg')",
+      },
+    },
+  },
+  plugins: [],
 }
