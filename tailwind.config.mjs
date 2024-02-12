c985d9550a6a63d15536fc691af1d3d3728b69b3
AlbeTorres <albertocorreoficial@gmail.com>
2024-02-12 18:32:57 -0500

    fix: vite error at get static img on build time

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
@@ -6,8 +7,8 @@ export default {
     extend: {
       backgroundColor: { graydark: '#0d1117' },
       backgroundImage: {
-        'open-menu': "url('./src/icons/open-menu.svg')",
-        'close-menu': "url('./src/icons/close-menu.svg')",
+        'open-menu': "url('/open-menu.svg')",
+        'close-menu': "url('/close-menu.svg')",
       },
     },
   },
